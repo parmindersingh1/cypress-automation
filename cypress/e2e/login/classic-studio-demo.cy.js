d62b5d18abdad06e8586afc62e1a2bcf7b0d0d22
Parminder Singh <parminder@racloop.com>
2022-10-05 04:08:06 -0700

    refactor code

--- cypress/e2e/login/classic-studio-demo.cy.js
+++ cypress/e2e/login/classic-studio-demo.cy.js
@@ -1,10 +1,9 @@
 /// <reference types="cypress" />
 import 'cypress-v10-preserve-cookie'
 
-describe("Login page", () => {
+describe("Classic Studio Demo", () => {
     before(() => {
         cy.login()
-       
     });
 
     beforeEach(() => {
@@ -21,15 +20,14 @@ describe("Login page", () => {
         cy.get('[data-cy="user-settings-button"] > .gap-3').click()
         // change user setting
         cy.get('[style="position: fixed; z-index: 1300; inset: 0px;"] > .MuiPaper-root > .rounded > :nth-child(1) > .w-full').click()
-        // cy.wait(5000)
+
         cy.visit("/user/settings").then(window => {
             cy.stub(window, 'open').as('open');
             cy.get('.MuiSvgIcon-root').click({ multiple: true })
-            // cy.wait(3000)
-            // cy.get('#toggle').check({force:true})
             cy.get('span').contains('Zsl lf demo').closest('li').find('#toggle').check({ force: true })
         });
 
+       
     })
 })
 
